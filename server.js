b5adfc8967799a946476c943169cd202cf011ae7
Steve <pyk3201k@gamil.com>
2019-12-12 19:29:37 +0800

    Auth Middle&Protecting Routes

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+//Connect Database
+connectDB();
+
+//init Middleware
+app.use(express.json({extended:false}));
 
 app.get('/',(req,res)=> 
   res.json({msg:'Welcome to the ContactKeeper API'})
@@ -13,4 +20,4 @@ app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;  
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`)); 
